4c5fd8f1ce4ad2e64a15008a53d269d026ac38bc
Martin WAROUX Ptolémée <waroux@ptolemee.com>
2023-06-16 11:59:19 +0200

    maj mentions légales

--- src/pages/FutureLegal.js
+++ src/pages/FutureLegal.js
@@ -4,15 +4,14 @@ import TtsButton from "../components/Button/TtsButton";
 const FutureLegal = () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
   return (
-    
     <div className="px-2 md:container py-8 space-y-8">
       {/* <TtsButton /> */}
 
       <div className="">
         <h2 className="text-secondary dark:text-black gotham-bold text-3xl mb-4">
-          Mentions légales
+        Mentions légales
         </h2>
-        
+       
         <p className="text-secondary dark:text-black text-xl mb-4">
         <bold>L’Agence de la biomédecine</bold>, agence nationale de l’État créée par la loi de bioéthique de 2004 modifiée en 2011 et 2013, 
         en qualité de responsable du traitement des données personnelles (encadrée par la loi « Informatique & Libertés » 
@@ -22,7 +21,6 @@ const FutureLegal = () => {
         Nous respectons la vie privée de tous ceux qui utilisent le site Web, rencontres-biomedecine.fr et ne collectons et utilisons les données personnelles 
         d'une manière qui est décrite ici, et d'une manière qui est compatible avec nos obligations et vos droits en vertu de la loi. 
         Cette Politique de Protection des Données Personnelles ne s'applique qu'à votre utilisation de notre site.
-
         </p>
       </div>
 
@@ -36,7 +34,6 @@ const FutureLegal = () => {
         fonction, pays, région, informations sur vos temps de participations aux rencontres.
         <br /> <br />Conformément au Règlement général sur la protection des données (RGPD), 
         les données personnelles désignent toute information pouvant vous identifier directement ou indirectement.
-
         </p>
       </div>
 
@@ -47,50 +44,36 @@ const FutureLegal = () => {
         <p className="text-secondary dark:text-black text-xl mb-4">
         En vertu du RGPD, nous devons toujours avoir une base légale pour l'utilisation des données personnelles. 
         Cela peut être parce que les données sont nécessaires ou parce qu'il est dans notre intérêt commercial légitime de les utiliser.
-
         <br /><br />Vos données personnelles peuvent être utilisées à des fins suivantes :
         <br />Réaliser le suivi des participations aux rencontres ;
         <br />• Être en conformité avec les obligations légales et réglementaires applicables à l’Agence de la biomédecine
         <br />• Nous ne traitons pas les données d'une manière incompatible avec les objectifs décrits ci-dessus.
-
         </p>
       </div>
 
       <div className="">
-        <h4 className="text-secondary dark:text-black gotham-bold text-[20px] mb-4">
+        <h4 className="text-secondary dark:text-black gotham-bold text-[23px] mb-4">
         Quels sont vos droits ?
         </h4>
         <p className="text-secondary dark:text-black text-xl mb-4">
         Vous avez le droit de demander l'accès et de recevoir des informations sur vos données personnelles ; 
         mettre à jour, corriger ou effacer vos données personnelles ; limiter ou vous opposer (pour des raisons légitimes) 
         au traitement de vos données personnelles auprès du Délégué de protection des données (« DPO ») 
-        de l’Agence de la biomédecine :
-        </p>
-      </div>
-
-      <div className="">
-        <h4 className="text-secondary dark:text-black gotham-bold text-[20px] mb-4">
-        Quels sont vos droits ?
-        </h4>
-        <p className="text-secondary dark:text-black text-xl mb-4">
-        Vous avez le droit de demander l'accès et de recevoir des informations sur vos données personnelles ; 
-        mettre à jour, corriger ou effacer vos données personnelles ; limiter ou vous opposer (pour des raisons légitimes) 
-        au traitement de vos données personnelles auprès du Délégué de protection des données (« DPO ») 
-        de l’Agence de la biomédecine :
-        <br />•	par courriel à l’adresse : <a
+        de l’Agence de la biomédecine : 
+        <br />• par courriel à l’adresse : <a
               href="mailto: dpo@biomedecine.fr " className={`text-primary dark:text-black mb-4`}>dpo@biomedecine.fr</a>
-        <br />•	par courrier postal, à l’adresse suivante :
+        <br />• par courrier postal, à l’adresse suivante :
         <br />Délégué à la protection des données
         <br />Direction juridique de l’Agence de la biomédecine 
         <br />1 avenue du Stade de France
         <br />93212 SAINT DENIS LA PLAINE Cedex
-
         <br /><br />Si vous avez des raisons de vous plaindre de notre utilisation de vos données personnelles, 
         vous avez le droit de déposer une plainte auprès de l'autorité compétente de protection des données de votre pays.
+        </p>
       </div>
-        
+
       <div className="">
-        <h4 className="text-secondary dark:text-black gotham-bold text-[20px] mb-4">
+        <h4 className="text-secondary dark:text-black gotham-bold text-[23px] mb-4">
         Combien de temps conservons-nous vos données personnelles ?
         </h4>
         <p className="text-secondary dark:text-black text-xl mb-4">
@@ -104,21 +87,7 @@ const FutureLegal = () => {
       </div>
 
       <div className="">
-        <h4 className="text-secondary dark:text-black gotham-bold text-[20px] mb-4">
-        Combien de temps conservons-nous vos données personnelles ?
-        </h4>
-        <p className="text-secondary dark:text-black text-xl mb-4">
-        Vos données à caractère personnel recueillies par l’Agence de la biomédecine sont conservées 
-        sous une forme qui permet votre identification juste pendant le temps nécessaire aux fins pour lesquelles 
-        les données à caractère personnel sont traitées. Plus précisément : 
-
-        <br />• Les données à caractère personnel recueillies pour le suivi des participations sont conservées 1 mois après la fin de la disponibilité de l’événement ;
-
-        </p>
-      </div>
-
-      <div className="">
-        <h4 className="text-secondary dark:text-black gotham-bold text-[20px] mb-4">
+        <h4 className="text-secondary dark:text-black gotham-bold text-[23px] mb-4">
         Comment sécurisons-nous vos données personnelles ?
         </h4>
         <p className="text-secondary dark:text-black text-xl mb-4">
@@ -126,28 +95,25 @@ const FutureLegal = () => {
         raisonnables pour garder vos données personnelles que nous recueillons et détenons confidentielles et pour les protéger 
         contre la divulgation ou l'accès non autorisé ou illégal, la perte accidentelle, destruction, altération ou dommages 
         en tenant compte de l'état de l'art de la technologie et du coût de mise en œuvre.
-
         </p>
       </div>
 
       <div className="">
-        <h4 className="text-secondary dark:text-black gotham-bold text-[20px] mb-4">
+        <h4 className="text-secondary dark:text-black gotham-bold text-[23px] mb-4">
         Partageons-nous vos données personnelles ?
         </h4>
         <p className="text-secondary dark:text-black text-xl mb-4">
         Vos données personnelles traitées par l’Agence de la biomédecine ne seront accessibles que par une liste limitée 
         de destinataires sur la base d'un besoin de savoir ou lorsque la loi l'exige, y compris, mais sans s'y limiter :
 
-        <br />•	Aux services et personnels de l’Agence de la biomédecine ; 
-        <br />•	Aux fournisseurs et prestataires de services de l’Agence de la biomédecine, notamment ceux en charge des inscriptions des participants aux rencontres de la biomédecine à des fins strictement nécessaires à l’exercice de leur mission ;
-        <br />•	Aux autorités compétentes telles que les autorités de santé dans certains cas définis par la législation.
-
-
+        <br />• Aux services et personnels de l’Agence de la biomédecine ; 
+        <br />• Aux fournisseurs et prestataires de services de l’Agence de la biomédecine, notamment ceux en charge des inscriptions des participants aux rencontres de la biomédecine à des fins strictement nécessaires à l’exercice de leur mission ;
+        <br />• Aux autorités compétentes telles que les autorités de santé dans certains cas définis par la législation.
         </p>
       </div>
 
       <div className="">
-        <h4 className="text-secondary dark:text-black gotham-bold text-[20px] mb-4">
+        <h4 className="text-secondary dark:text-black gotham-bold text-[23px] mb-4">
         Droit et juridiction
         </h4>
         <p className="text-secondary dark:text-black text-xl mb-4">
@@ -155,15 +121,10 @@ const FutureLegal = () => {
         Tout différend sera porté devant les tribunaux compétents de Paris (France).
         L’application des règles de conflit de lois, qui restreindrait la pleine application de la loi française, est, par la présente, exclue. 
         En conséquence, la loi française s’applique à tout Utilisateur qui navigue sur le Site et qui utilisent tout ou partie de ses fonctionnalités.
-
-
-
         </p>
       </div>
 
-       
-
-      </div>
+    </div>
   );
 };
 
